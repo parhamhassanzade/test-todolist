8bfe4b4714f5d95cd88849b2ed0bfe33a905f527
parham <parhamhassanzade.p@gmail.com>
2022-12-01 12:38:16 +0330

    complete Ui

--- component/Header.js
+++ component/Header.js
@@ -7,33 +7,30 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import FactCheckOutlinedIcon from "@mui/icons-material/FactCheckOutlined";
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import Link from "next/link";
+
+const pages = ["AboutUS", "Contact"];
 
 export default function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  //   const handleOpenUserMenu = (event) => {
+  //     setAnchorElUser(event.currentTarget);
+  //   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  //   const handleCloseUserMenu = () => {
+  //     setAnchorElUser(null);
+  //   };
 
   return (
     <AppBar position="static">
@@ -96,7 +93,9 @@ export default function Header() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <FactCheckOutlinedIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -113,48 +112,31 @@ export default function Header() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            TodoApp
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
+              <Link href={`/${page}`}>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              </Link>
             ))}
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              gap: "30px",
+            }}
+          >
+            <Typography>All your Todo:</Typography>
+            <Typography>Finished:</Typography>
+            <Typography>NotFinished:</Typography>
           </Box>
         </Toolbar>
       </Container>
--- pages/Todo/index.js
+++ pages/Todo/index.js
@@ -1,13 +1,58 @@
-import React from "react";
+import { useState } from "react";
 import Head from "next/head";
 import Layout from "../../component/Layout";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import CardItem from "../../component/cardItem";
+
 function index() {
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
   return (
     <>
       <Head>
         <title>ToDoList</title>
       </Head>
-      <Layout>{<p>Todo Page</p>}</Layout>
+      <Layout>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            p: 1,
+          }}
+        >
+          {" "}
+          <FormControl fullWidth sx={{ m: 1 }}>
+            <TextField
+              variant="outlined"
+              label="new task...."
+              value={values.amount}
+              onChange={handleChange("amount")}
+            />
+          </FormControl>
+        </Box>
+        <Box
+          sx={{ display: "flex", justifyContent: "center", gap: "15px", p: 2 }}
+        >
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+        </Box>
+      </Layout>
     </>
   );
 }
